314a6ac6b67c846e0081fbbb303479cb1d38a222
Faizan_Vhora <faizanvhora999@gmail.com>
2026-02-23 10:52:52 +0530

    Add header, hero, footer with routing and 404 page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import ComingSoon from './pages/ComingSoon';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <ComingSoon />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
